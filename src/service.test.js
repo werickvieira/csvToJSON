6412e25d45ca1c9e812d7124fad1e6a6493fb67f
Wérick Vieira <werickvs@gmail.com>
2020-08-08 22:05:42 -0300

    feat: iniciando os testes do service

---
+++ src/service.test.js
@@ -0,0 +1,8 @@
+import service from '../src/service';
+
+describe('function service', () => {
+  const _ = service();
+  it('should failed when detected have an empty argument', () => {
+    // expect(m.detectSeparator()).toHaveBeenCalled();
+  });
+});
